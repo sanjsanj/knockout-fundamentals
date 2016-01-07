3f8721bf1356b5df45e4a2ed516bafbed0fdc679
Sanjay Purswani <sanjsanj@hotmail.com>
2016-01-07 12:18:33 +0000

    Display an array through an observableArray

--- Project1/script/hello.js
+++ Project1/script/hello.js
@@ -1,14 +1,21 @@
 // JavaScript source code
 
 $(function () {
+    var data = [{ name: 'Bob' }, { name: 'Tim' }, { name: 'Tom' }];
     var viewModel = {
         name: ko.observable('Bob'),
         changeName: function () {
             this.name('Steve');
         },
-        nameVisible: ko.observable(true)
+        nameVisible: ko.observable(true),
+        list: ko.observableArray(data),
+        addItem: function () {
+            this.list.push({ name: 'Steve' });
+        },
+        removeItem: function () {
+            this.list.pop();
+        }
     };
-
     viewModel.displayName = ko.computed(function () {
         return this.name() + ' is ' + (!this.nameVisible() ? 'not ' : '') + 'visible';
     }, viewModel);
